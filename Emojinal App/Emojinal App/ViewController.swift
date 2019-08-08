1f2f00c16ec47279f15c917a4f7036df4dee9908
Apple <apple@Apples-MacBook-Pro-107.local>
2019-08-08 16:42:46 -0400

    get it tom

--- Emojinal App/Emojinal App/ViewController.swift
+++ Emojinal App/Emojinal App/ViewController.swift
@@ -14,7 +14,16 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func showMessage(sender: UIButton) {
+        // in as much detail as possible
+        // pseudo-code the things that need to happen
+        // when a user clicks an emoji button
+      let alertController = UIAlertController(title: "Your Superhero is...", message: "batman", preferredStyle: UIAlertController.Style.alert)
+        
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        
+        present(alertController, animated: true, completion: nil)
+        
+    }
+   
 }
-
